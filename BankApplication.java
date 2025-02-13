edae487bbc600dee5a63be8f95a87fd622265663
brolince-larry <brolincelarry@gmail.com>
2025-02-13 23:59:39 +0300

    BankApplication.java

--- BankApplication.java
+++ BankApplication.java
@@ -1,33 +1,38 @@
+// Abstract class representing a bank account
 abstract class BankAccount {
-
     protected double balance;
 
+    // Constructor to initialize the balance
     public BankAccount(double balance) {
         this.balance = balance;
     }
 
+    // Method to deposit an amount to the account
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
-        } else {
-            System.out.println("Error! Amount should be positive");
         }
     }
 
+    // Abstract method for withdrawal, to be implemented by subclasses
     public abstract void withdraw(double amount);
 
+    // Method to get the current balance
     public double getBalance() {
         return balance;
     }
 }
 
+// Subclass representing a savings account
 class SavingsAccount extends BankAccount {
-    private static final double TRANSACTION_FEE = 1.0;
+    private static final double TRANSACTION_FEE = 1.0; // Transaction fee for withdrawals
 
+    // Constructor to initialize the balance
     public SavingsAccount(double balance) {
         super(balance);
     }
 
+    // Implementation of the withdraw method for savings account
     @Override
     public void withdraw(double amount) {
         double totalAmount = amount + TRANSACTION_FEE;
@@ -39,11 +44,15 @@ class SavingsAccount extends BankAccount {
     }
 }
 
+// Subclass representing a checking account
 class CheckingAccount extends BankAccount {
+
+    // Constructor to initialize the balance
     public CheckingAccount(double balance) {
         super(balance);
     }
 
+    // Implementation of the withdraw method for checking account
     @Override
     public void withdraw(double amount) {
         if (amount > 0 && amount <= balance) {
@@ -54,16 +63,19 @@ class CheckingAccount extends BankAccount {
     }
 }
 
+// Main class to test the bank accounts
 public class BankApplication {
     public static void main(String[] args) {
+        // Create a SavingsAccount object with an initial balance of 1000
         SavingsAccount savingsAccount = new SavingsAccount(1000);
-        savingsAccount.deposit(200);
-        savingsAccount.withdraw(100);
+        savingsAccount.deposit(200); // Deposit 200 to the savings account
+        savingsAccount.withdraw(100); // Withdraw 100 from the savings account
         System.out.println("Savings Account Balance: " + savingsAccount.getBalance());
 
+        // Create a CheckingAccount object with an initial balance of 1000
         CheckingAccount checkingAccount = new CheckingAccount(1000);
-        checkingAccount.deposit(200);
-        checkingAccount.withdraw(100);
+        checkingAccount.deposit(200); // Deposit 200 to the checking account
+        checkingAccount.withdraw(100); // Withdraw 100 from the checking account
         System.out.println("Checking Account Balance: " + checkingAccount.getBalance());
     }
 }
